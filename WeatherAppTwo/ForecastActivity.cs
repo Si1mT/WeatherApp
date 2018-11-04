107b79565d2a4e98f0bc20b82039f2b9cf861984
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-11-05 00:23:00 +0200

    Revert "started new layout"
    This reverts commit a6bc38660d2e6f4fb050636efcb51b3f98082951.

--- WeatherAppTwo/ForecastActivity.cs
+++ WeatherAppTwo/ForecastActivity.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.App;
+using Android.OS;
 using Android.Support.V7.App;
 
 namespace WeatherAppTwo
@@ -21,40 +24,7 @@ namespace WeatherAppTwo
             SetContentView(Resource.Layout.activity_forecast);
             // Create your application here
 
-        }
-    }
 
-    public class CustomAdapter : BaseAdapter<string>
-    {
-        List<string> items;
-        Activity context;
-
-        public CustomAdapter(Activity context, List<string> items) : base()
-        {
-            this.context = context;
-            this.items = items;
-        }
-
-        public override string this[int position]
-        {
-            get { return items[position]; }
-        }
-
-        public override int Count { get { return items.Count; } }
-
-        public override long GetItemId(int position)
-        {
-            return position;
-        }
-
-        public override View GetView(int position, View convertView, ViewGroup parent)
-        {
-            View view = convertView;
-            if (view == null)
-                view = context.LayoutInflater.Inflate(Resource.Layout.activity_forecast, null);
-
-            view.FindViewById<TextView>(Resource.Id.textView1).Text = items[position];
-            return view;
         }
     }
 }
